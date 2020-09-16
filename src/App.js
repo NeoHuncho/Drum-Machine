4f63fa6f13a37be972f35f3fa048e13f4f3d8d43
NeoHuncho <william.guinaudie@gmail.com>
2020-09-16 13:47:43 +0200

    added grid system

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import DrumPads from './components/DrumPads';
+import Controls from './components/Controls';
 
 function App() {
   return (
     <div className="App">
-      
+      <DrumPads />
+      <Controls />
     </div>
   );
 }
---
+++ src/components/DrumPads.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './css/DrumPads.css';
+
+
+export class drum_Pads extends Component {
+    render() {
+        return (
+         <section class="drums">
+            <div class="pad">q</div>
+            <div class="pad">w</div>
+            <div class="pad">e</div>
+            <div class="pad">a</div>
+            <div class="pad">s</div>
+            <div class="pad">d</div>
+            <div class="pad">z</div>
+            <div class="pad">x</div>
+            <div class="pad">c</div>
+        </section>
+        )
+    }
+}
+
+export default drum_Pads
