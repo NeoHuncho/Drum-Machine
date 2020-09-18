cf59b033e285443dfd6b6c2e305ad7ec7f709eff
NeoHuncho <william.guinaudie@gmail.com>
2020-09-18 11:13:13 +0200

    added sound to buttons

--- src/components/DrumPads.js
+++ src/components/DrumPads.js
@@ -98,20 +98,22 @@ const bankTwo = [{
 
 export class drum_Pads extends Component {
     playSound=(e)=>{
-    
+  let result=bankOne.filter(element => e.target.getAttribute('name')===element.keyTrigger).map(element=> element.url)
+  let sound= new Audio(result);
+  sound.play()
     }
     render() {
         return (
          <section class="drums">
-            <div class="pad" onClick={this.playSound}>q</div>
-            <div class="pad">w</div>
-            <div class="pad">e</div>
-            <div class="pad">a</div>
-            <div class="pad">s</div>
-            <div class="pad">d</div>
-            <div class="pad">z</div>
-            <div class="pad">x</div>
-            <div class="pad">c</div>
+            <div class="pad" name="Q" onClick={this.playSound}>q</div>
+            <div class="pad" name="W" onClick={this.playSound}>w</div>
+            <div class="pad" name="E" onClick={this.playSound}>e</div>
+            <div class="pad" name="A" onClick={this.playSound}>a</div>
+            <div class="pad" name="S" onClick={this.playSound}>s</div>
+            <div class="pad" name="D" onClick={this.playSound}>d</div>
+            <div class="pad" name="Z" onClick={this.playSound}>z</div>
+            <div class="pad" name="X" onClick={this.playSound}>x</div>
+            <div class="pad" name="C" onClick={this.playSound}>c</div>
         </section>
         )
     }
