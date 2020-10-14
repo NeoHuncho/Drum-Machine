e8fb237dba6e3ca73df42b4486180e1995c9a91a
NeoHuncho <william.guinaudie@gmail.com>
2020-10-14 02:33:14 +0200

    added animations

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import DrumPads from './components/DrumPads';
 import Controls from './components/Controls';
 import { Provider } from 'react-redux';
 import Store from './store'
+import {motion} from 'framer-motion'
 function App() {
   return (
     <Provider store={Store} >
-    <div className="App" id="drum-machine">
+    <motion.div className="App" id="drum-machine"
+    initial={{opacity:0, x:'10vw'}}
+    animate={{opacity:1,x:0}}
+    transition={{duration:1}}>
       <DrumPads />
       <Controls />
-    </div>
+    </motion.div>
     </Provider>
   );
 }
